8396a0967d96be571a8970f536d52cfa124a16e5
Quinten <quinten.roets@gmail.com>
2022-10-08 13:25:36 -0400

    don't make deadlines full day because it is less visible in google calendar

--- canvasdl/utils/calendar.py
+++ canvasdl/utils/calendar.py
@@ -1,20 +1,14 @@
 import os
-from datetime import datetime, time, timedelta
+from datetime import datetime
 
 from gcsa.google_calendar import Event, GoogleCalendar
 
 from .path import Path
 
-DAY = timedelta(days=1)
-
 
 def add_todo(message, date):
-    event_time = date.time()
-    full_day = event_time == time(hour=23, minute=59)
-    start = date - timedelta(hours=23, minutes=59) if full_day else date
-    end = start + DAY if full_day else start
-    if end.replace(tzinfo=None) > datetime.now():
-        add_event(message, start, end)
+    if date.replace(tzinfo=None) > datetime.now():
+        add_event(message, date, date)
 
 
 def add_event(message, start, end):
