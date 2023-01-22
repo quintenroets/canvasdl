15f52bb3f1fcacc139ad9c5113ff81850f63e01f
Quinten <quinten.roets@gmail.com>
2023-01-22 14:40:20 -0500

    adapt to updated superpathlib

--- canvasdl/core/courseinfo.py
+++ canvasdl/core/courseinfo.py
@@ -5,8 +5,6 @@ from canvasdl.utils import Path
 def get_courses():
     from . import coursemaker  # noqa: autoimport
 
-    courses = (
-        Path.courses.load() if Path.courses.exists() else coursemaker.make_courses()
-    )
+    courses = Path.courses.yaml or coursemaker.make_courses()
     courses = Courses.from_dict(courses)
     return courses.courses
